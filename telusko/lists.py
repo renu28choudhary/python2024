ebd472643a6e6effa6db4d960a8deeb3abe75517
renu28choudhary <renu28choudhary@gmail.com>
2024-10-05 21:50:55 -0400

    learning python basics

---
+++ telusko/lists.py
@@ -0,0 +1,17 @@
+num =[2,3,4,5]
+print(num[0])
+print(num[0:2])
+num.append(6)
+print(num)
+num.pop()
+print(num)
+del num[2:]
+print(num)
+num.extend([9,8,7])
+print(num)
+print(min(num))
+print(max(num))
+print(sum(num))
+print(num)
+num.sort()
+print(num)
